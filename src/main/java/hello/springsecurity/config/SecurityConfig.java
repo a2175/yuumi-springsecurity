50f9c1896890d8866e59dded9df5c3e87db902db
a2175 <as217512@gmail.com>
2024-10-13 23:17:14 +0900

    스프링 시큐리티 4 : 커스텀 로그인 설정
    https://www.youtube.com/watch?v=eEkV0zir9mQ&list=PLJkjrxxiBSFCKD9TRKDYn7IE96K2u3C3U&index=5

--- src/main/java/hello/springsecurity/config/SecurityConfig.java
+++ src/main/java/hello/springsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,6 +21,15 @@ public class SecurityConfig {
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
                 );
+
+        http
+                .formLogin((auth) -> auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
+                );
+
+        http
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
